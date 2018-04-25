68e237db143cb4acb16c1d7932c7a503c5c79654
Jochen Metzger <j.metzger@t3company.de>
2018-04-25 10:43:55 +0200

    Go ahead

---
+++ HelloWorld4/src/MyPackage/HelloWorld.java
@@ -0,0 +1,12 @@
+package MyPackage;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // test it 
+		System.out.println("abc");
+		
+	}
+
+}
